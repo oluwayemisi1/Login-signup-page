84db5ad071ad434a6d1354aa0aa7b2a9396936b9
Oluwayemisi Jaiyeoba <yemijaiyeoba20@gmail.com>
2025-06-03 16:17:34 -0700

    Initial commit

---
+++ Login_app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import LoginSignup from './Components/LoginSignup';
+
+function App() {
+
+  return (
+    <div>
+      <LoginSignup/>
+
+    </div>
+  )
+}
+
+export default App;
